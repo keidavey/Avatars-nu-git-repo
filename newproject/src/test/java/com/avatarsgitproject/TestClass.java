4b75c0bfd898f249d4f77c3396b7b1c4704f1800
kdavey <keindavey@gmail.com>
2024-07-01 18:53:51 -0400

    change from flop

--- newproject/src/test/java/com/avatarsgitproject/TestClass.java
+++ newproject/src/test/java/com/avatarsgitproject/TestClass.java
@@ -5,6 +5,10 @@ public class TestClass {
     public static void main(String[] args) {
 
         System.out.println("Hello");
+        String string1 = "change from checkout branch";
+        String string2 = "change from flop branch";
+
+
     }
 
 }
